bf21bc6873ff7ef7f1acd3a678054a7833beefec
Tati <86420885+Tati-Ramos@users.noreply.github.com>
2021-11-24 15:42:38 -0300

    Commit

--- src/main/java/com/projeto/listaencadeada/Main.java
+++ src/main/java/com/projeto/listaencadeada/Main.java
@@ -15,6 +15,15 @@ public class Main {
         System.out.println(minhaListaEncadeada.get(2));
         System.out.println(minhaListaEncadeada.get(3));
 
+        System.out.println(minhaListaEncadeada);
+
+        System.out.println(minhaListaEncadeada.remove(3));
+        System.out.println(minhaListaEncadeada);
+
+
+
+
+
     }
 
 }
