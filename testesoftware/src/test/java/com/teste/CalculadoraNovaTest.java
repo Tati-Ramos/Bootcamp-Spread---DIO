90bc030a385a1637b25c1a468ca9fcafa39ededc
Tati <86420885+Tati-Ramos@users.noreply.github.com>
2021-12-21 09:00:12 -0300

    first commit

--- testesoftware/src/test/java/com/teste/CalculadoraNovaTest.java
+++ testesoftware/src/test/java/com/teste/CalculadoraNovaTest.java
@@ -19,3 +19,14 @@ public class CalculadoraNovaTest {
     }
 
 }
+
+
+/*
+O que vem a ser a sigla TDD?
+
+TDD é uma sigla para Test Driven Development, ou Desenvolvimento Orientado a Testes. A ideia do TDD é que você trabalhe em ciclos. Estes ciclos ocorrem na seguinte ordem:
+
+Primeiro, escreva um teste unitário que inicialmente irá falhar, tendo em vista que o código ainda não foi implementado;
+Crie o código que satisfaça esse teste, ou seja: implemente a funcionalidade em questão. Essa primeira implementação deverá satisfazer imediatamente o teste que foi escrito no ciclo anterior;
+Quando o código estiver implementado e o teste satisfeito, refatore o código para melhorar pontos como legibilidade. Logo após, execute o teste novamente. A nova versão do código também deverá passar sem que seja necessário modificar o teste escrito inicialmente.
+ */
